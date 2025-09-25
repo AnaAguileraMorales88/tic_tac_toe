c01fc5111ea33d3fb968f706662ec4e1bd4b5c3f
Paola <mpulgav@gmail.com>
2025-09-25 11:59:23 +0200

    make move

--- src/main/java/com/tic_tac_toe/Board.java
+++ src/main/java/com/tic_tac_toe/Board.java
@@ -13,17 +13,29 @@ public class Board {
 
     }
     public void printBoard() {
-        System.out.println(" 0 1 2");
+        System.out.println("    0    1    2  ");
+        System.out.println("  -----+----+-----");
         for (int i = 0; i < 3; i++){
-            System.out.print(i + " ");
+            System.out.print(i + " | ");
             for (int j = 0; j < 3; j++) {
-                System.out.print(grid[i][j].getValue());
-                if (j < 2) System.out.print("|");
+                char val = grid [i][j].getValue();
+                String contString = (val == ' ') ? " " : String.valueOf(val);
+                System.out.print(" " + contString + " | ");
             }
             System.out.println();
-            if (i < 2 ) System.out.println("  -----");   
+            if (i < 2 ) System.out.println("  -----+----+-----");   
     }
 }
-}
+    public boolean makeMove(Coordinate coor, char player){
+        if (grid[coor.getRow()][coor.getColumn()].isEmpty()){
+           grid[coor.getRow()][coor.getColumn()].setValue(player);
+           
+           return true;
+        }
+           return false;
+    }
 
+
+
+}
 
