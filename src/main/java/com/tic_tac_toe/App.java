d02c60c9f892b0c7fadc470bf1d54eb99b831d8d
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-09-25 13:40:19 +0200

    add dialogue and functionality

--- src/main/java/com/tic_tac_toe/App.java
+++ src/main/java/com/tic_tac_toe/App.java
@@ -1,14 +1,45 @@
 package com.tic_tac_toe;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-       Board board = new Board();
-       board.printBoard();
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Board board = new Board();
+        char currentPlayer = 'X';
+
+        System.out.println("Bienvenid@ a nuestro juego!");
+  
+        board.printBoard();
+
+        while (true) {
+            System.out.println("Turno de jugad@r: " + currentPlayer);
+            System.out.print("Ingresa fila (0-2): ");
+            int row = sc.nextInt();
+            System.out.print("Ingresa columna (0-2): ");
+            int column = sc.nextInt();
+
+            Coordinate move = new Coordinate(row, column);
+            if(!board.makeMove(move, currentPlayer)){
+                System.out.println("Esa casilla ya está ocupada, intentálo de nuevo");
+                continue;
+            }
+            board.printBoard();
+
+            if(board.checkWinner(currentPlayer)){
+                System.out.println(currentPlayer + " ha ganado!!");
+                break;
+            }
+
+            if(board.isFull()){
+                System.out.println("Empate. No hay más movimientos posibles.");
+                break;
+            }
+
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+
+        }
+            sc.close();
+     
     }
 }
--- src/main/java/com/tic_tac_toe/Box.java
+++ src/main/java/com/tic_tac_toe/Box.java
@@ -1,17 +1,21 @@
 package com.tic_tac_toe;
 
 public class Box {
-    private char value = ' ';  
+    private char value;
 
-    public boolean isEmpty() {
-        return value == ' ';
+    public Box() {
+        this.value = ' '; 
+    }
+
+    public char getValue() {
+        return value;
     }
 
     public void setValue(char value) {
         this.value = value;
     }
 
-    public char getValue() {
-        return value;
+    public boolean isEmpty() {
+        return value == ' ';
     }
 }
